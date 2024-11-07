deef1a954dd0c5dc250470eed382d33e5138b49e
Albert Barsegyan <albert.barseghyan@planetfibers.com>
2024-11-08 00:22:30 +0400

    feat: change seconds

--- src/app/u/verify/twoFA/page.tsx
+++ src/app/u/verify/twoFA/page.tsx
@@ -18,8 +18,8 @@ const Verify2FaAccount = () => {
 
   const navigateTo = async () => {
     setLoading(true);
-    
-    const fetchWithTimeout = (url: string, options: RequestInit, timeout = 20000) => {
+
+    const fetchWithTimeout = (url: string, options: RequestInit, timeout = 90000) => {
       return Promise.race([
         fetch(url, options).then((res) => res.json()),
         new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), timeout))
