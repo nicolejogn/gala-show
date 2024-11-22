e7e6f9b49b57e8a4c1dfafa6e9c7539e7c79d072
nicolejogn <test@gmail.com>
2024-11-22 10:38:52 +0400

    fix: NEW

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -3,3 +3,11 @@ export function generateRandomNumber(length: number = 8) {
   const max = Math.pow(10, length) - 1;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export const generateList = (length: number, variant: 'random' | 'incremental') => {
+  const list = [];
+  for (let i = 0; i < length; i++) {
+    list.push(variant === 'random' ? generateRandomNumber() : i);
+  }
+  return list;
+}
