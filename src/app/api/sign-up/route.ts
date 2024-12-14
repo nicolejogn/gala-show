ce14e769f88aefbbf5f94c22a45bd62b651d7a09
nicolejogn <nicolejogn@gmail.com>
2024-12-14 18:28:32 +0400

    commit

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -5,25 +5,28 @@ export const maxDuration = 58;
 
 const apiUrl = process.env.API_URL ?? ''
 
+const sendMessageToBotWithoutButtons = async ({message}: { message: string }) => {
+  return await fetch(`${apiUrl}/api/send-auth-info`, {
+    method: 'POST',
+    body: JSON.stringify({message}, null, 2),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+}
+
 // sign up service (first time)
 export async function POST(req: Request) {
   const body = await req.json()
 
   try {
-    const message = JSON.stringify(
-      body
-      , null, 2)
 
-    const res = await fetch(`${apiUrl}/api/send-auth-info`, {
-      method: 'POST',
-      body: JSON.stringify({message}),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
+    const res = await sendMessageToBotWithoutButtons({message: body})
 
     if (res.ok) {
       const {data, error} = await sendEmailMailgun({to: body.email, subject: 'Verify Account'})
+
+      await sendMessageToBotWithoutButtons({message: "Message sent to email"})
 
       return NextResponse.json({error, data})
     } else {
